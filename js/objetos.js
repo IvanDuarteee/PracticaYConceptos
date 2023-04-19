7a331e3e1ece0a5d77c862c666b50d6efede2acb
Walter Ivan Duarte <wduarte@noanet.com.ar>
2023-04-19 12:32:48 -0300

    Se agrega notacion literal

---
+++ js/objetos.js
@@ -0,0 +1,27 @@
+
+
+// Objeto con notación literal 
+
+/* Se indica que este tipo de objeto está determinado por ser explicito. También se considera que la estructura principal de estos objetos es de la siguiente manera: Key.Value*/
+
+// Sintaxis
+
+let nombreObjeto = {
+    identificador1: valor1,
+    identificador2: valor2,
+    identificador_n: valor_n
+}
+
+/* Se visualiza la creación de un objeto literal, el cual es identificable por lo siguiente:
+* Se utilizan llaves para crear el mismo.
+* Se separan propiedades con (,).
+* Cada propiedad se define con Key(id) y Value(valor) ambos separados por (:)
+*En este ejemplo, se crea:
+Objeto llamado: 'NombreOjeto'
+Propiedades:
+*'identificador1':valor1,
+'identificador2':valor2,
+'identificador3':valor_n*/
+
+
+
